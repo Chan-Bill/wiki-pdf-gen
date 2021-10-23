e75ee4a7b9e4c43023d3f481ff552993781c9e9d
BillT <billtorrentira@gmail.com>
2021-10-23 18:18:08 +0800

    Remove loops inside the class & refactor it

--- pdf.py
+++ pdf.py
@@ -1,36 +1,32 @@
 from fpdf import FPDF
 
 
-class CreatePDF:
-    '''layout -> Page Layout, unit -> Unit Measurement,
-        format -> Kind of Paper, font -> font style,
-        chr -> font format, f_size -> font size
-    '''
+class CreatePDF(FPDF):
 
-    def __init__(self, layout, unit, format, font, chr, f_size):
-        self.layout = layout
-        self.unit = unit
-        self.format = format
-        self.font = font
-        self.chr = chr
-        self.f_size = f_size
+    def chapter_title(self, title):
+        # Setting font: Times 12
+        self.set_font("Times", size=12)
+        # Printing centered text:
+        self.cell(0, 10, title, ln=True, align='C')
+        # Performing a line break:
+        self.ln()
 
-    def writePage(self, cell_h, title, con, file_no, files_count):
+    def chapter_body(self, filepath):
+        # Reading text file:
+        with open(filepath, "rb") as fh:
+            txt = fh.read().decode("latin-1")
+        # Setting font: Times 12
+        self.set_font("Times", size=12)
+        # Printing justified text:
+        self.multi_cell(0, 5, txt)
+        # Performing a line break:
+        self.ln()
+        # Final mention in italics:
+        self.set_font(style="I")
+        self.cell(0, 5, "(end of excerpt)")
 
-        filename = []
-        for i in range(files_count + 1):
-            filename.append(f'file_{i}')
-
-        for i in filename:
-            locals()[i] = FPDF(self.layout, self.unit, self.format)
-            locals()[i].add_page()
-            locals()[i].set_font(self.font, self.chr, self.f_size)
-            locals()[i].cell(0, cell_h, title, ln=True, align='C')
-            locals()[i].cell(0, cell_h, 'Essay', ln=True, align='C')
-
-            locals()[i].multi_cell(0, cell_h, con, align='J', border=1)
-            return locals()[i].output(f'generated/English-{title}-Essay_{file_no + 1}.pdf')
-
+    def print_chapter(self, filepath):
+        self.chapter_body(filepath)
 
 # pan = CreatePDF('P', 'mm', 'Letter', 'times', '', 16)
 # pan.writePage(40, 10, 'hello world', 5, 6)
