ac677bc041ae5f726a94992061c3e57f45c6691d
BillT <billtorrentira@gmail.com>
2021-10-23 18:16:56 +0800

    Remove loops inside the class

--- wiki.py
+++ wiki.py
@@ -1,33 +1,35 @@
-import wikipediaapi
 import wikipedia
 
+
+class Open_File:
+
+    def __init__(self, filename, mode):
+        self.filename = filename
+        self.mode = mode
+
+    def __enter__(self):
+        self.file = open(self.filename, self.mode)
+        return self.file
+
+    def __exit__(self, exc_type, exc_val, traceback):
+        self.file.close()
+
+
 class WikiFetch:
-    '''This class will get the titles and content of each titles
+    '''This will pass a 'keyword' that will going to search.
     '''
 
     def __init__(self, keyword):
         self.keyword = keyword
-        self.fetched = []
 
     def getTitles(self):
-        return wikipedia.search(self.keyword)
+        self.title = wikipedia.search(self.keyword)
+        return self.title
 
     def getContent(self):
-
-        fetched = [ i.replace(' ','_')for i in self.getTitles()]
-        cont = []
-
-        for i in fetched:
-
-            wiki_wiki = wikipediaapi.Wikipedia('en')
-            page_py = wiki_wiki.page(i)
-            cont.append(page_py.summary[:1536])
-        
-        return cont
-        
-
-
+        self.fetched = [i.replace(' ','_')for i in self.getTitles()]
+        return self.fetched
 
 # pan = WikiFetch(keyword='Linux kernel')
 # print(pan.getTitles())
-# print(pan.getContent())
+# pan.getContent()
