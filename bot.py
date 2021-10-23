097959a86f6b047e87e46e92b5a38a0d03e15fc3
BillT <billtorrentira@gmail.com>
2021-10-23 18:20:54 +0800

    Refactor the code & loops should always inside this file

--- bot.py
+++ bot.py
@@ -1,24 +1,44 @@
+import wikipediaapi
 from wiki import WikiFetch
+from wiki import Open_File
 from pdf import CreatePDF
 
-keyword = 'Linux'
-# print('''
-# **** PDF GENERATOR FOR COURSEHERO UNLOCK ****
-# ****         Author : BillT BSME         ****
-# ''')
+print('''
+**** PDF GENERATOR FOR COURSEHERO UNLOCK ****
+****         Author : BillT BSME         ****
+''')
 
-# keyword = input('Enter Topic: ')
+keyword = input('Enter Topic: ')
 
 
-engine = WikiFetch(keyword)
-file = CreatePDF('P', 'mm', 'A4', 'helvetica', '', 12)
 
-titles = engine.getTitles()
-contents = engine.getContent()
+# Wikipedia Scrape
+# Creating wikifetch object
+crawler = WikiFetch(keyword)
+# Getting all the titles from the topic
+page_titles = crawler.getTitles()
+# Generating all the contents each title
+for i in crawler.getContent():
 
-for i in range(len(titles)):
-    file.writePage(5, titles[i], contents[i], i, len(titles))
+    wiki_wiki = wikipediaapi.Wikipedia('en')
+    page_py = wiki_wiki.page(i)
+    
+    with Open_File(f'body/sample_{i}.txt', 'w') as f:
+        f.write(page_py.summary)
 
+
+# Pdf Generation
+for i in page_titles:
+    # Creating createpdf object
+    locals()[i] = CreatePDF()
+    # Add page before writing
+    locals()[i].add_page()
+    # Printing the titles
+    locals()[i].chapter_title(i)
+    # Printing the body
+    locals()[i].print_chapter(f'body/sample_{i}.txt')
+    # Generate PDF file
+    locals()[i].output(f'generated/sample_{i}.pdf')
 
     
 
