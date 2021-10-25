a2915fde4c809fbfbb3a73a040e36ffeb14ff384
BillT <billtorrentira@gmail.com>
2021-10-25 09:16:10 +0800

    Detect network connection

--- bot.py
+++ bot.py
@@ -1,6 +1,7 @@
 import os
-import requests
 import shutil
+import platform
+import requests
 import wikipediaapi
 from time import sleep
 from wiki import WikiFetch
@@ -28,63 +29,91 @@ def loadbar(iteration, total, prefix='', suffix='', decimals=1, length=100, fill
 		print()
 
 
+
 # Welcome Page
 print('''
-**** PDF GENERATOR FOR COURSEHERO UNLOCK ****
-****  Author : BillT BSME  Version: 1.0  ****
+**** Random PDF Generator from Wikipedia ****
+****           Version: 1.2              ****
+
+Source Code: https://github.com/Christian-Bill/wiki-doc-gen
 ''')
 
 # Enter your topic of choice
 keyword = input('Enter Topic: ')
+print('How many files you want? Max = 50')
+query_count = int(input('Enter here: '))
 print('')
 
-# Wikipedia Object
-crawler = WikiFetch(keyword)
-# Getting all the titles from the topic
-page_titles = crawler.getTitles()
-# Generating all the contents each title
-page_content = crawler.getContent()
-# Summary iteration
-count_titles = len(page_content)
-loadbar(0, count_titles, prefix='PDF-Generating:', suffix='Complete', length=count_titles)
-for i in page_content:
+
+if query_count > 50:
+    print('Max = 50')
+    query_count = int(input('Enter here: '))
+    print('')
+
+else:
 
     try:
-        wiki_wiki = wikipediaapi.Wikipedia('en')
-        page_py = wiki_wiki.page(i)
-        
-        with Open_File(f'body/sample_{page_content.index(i)}.txt', 'w') as f:
-            f.write(page_py.summary)
+        # Wikipedia Object
+        crawler = WikiFetch(keyword, query_count)
+        # Getting all the titles from the topic
+        page_titles = crawler.getTitles()
+        # Generating all the contents each title
+        page_content = crawler.getContent()
+        # Summary iteration
+        count_titles = len(page_content)
+        loadbar(0, count_titles, prefix=f'Generating {query_count} files:', suffix='Complete', length=count_titles)
+        for i in page_content:
+
+            try:
+                wiki_wiki = wikipediaapi.Wikipedia('en')
+                page_py = wiki_wiki.page(i)
+                
+                with Open_File(f'body/sample_{page_content.index(i)}.txt', 'w') as f:
+                    f.write(page_py.summary)
+
+            except requests.exceptions.ConnectionError:
+                shutil.rmtree('body')
+                print('Something else went wrong')
+            
+            sleep(0.1)
+            loadbar(page_content.index(i) + 1, count_titles, prefix=f'Generating {query_count} files:', suffix='Complete', length=count_titles)
+
+
+
+        # Pdf Generation
+        for i in page_titles:
+            # Creating createpdf object
+            locals()[i] = CreatePDF()
+            # Add page before writing
+            locals()[i].add_page()
+            # Printing the titles
+            locals()[i].chapter_title(i)
+            # Printing the body
+            locals()[i].print_chapter(f'body/sample_{page_titles.index(i)}.txt')
+            # Generate PDF file
+            locals()[i].output(f'generated/Eng-Essay_{i}.pdf')
+
+        # Delete body files
+        shutil.rmtree('body')
+
+        # Closing Message
+        current_dir = os.getcwd()
+        if platform.system() == 'Linux' or 'Darwin':
+            print(f'Generated PDF files : {current_dir}/generated')
+            print('')
+            input('Press ENTER to exit')
+        else:
+            print(f'Generated PDF files : {current_dir}\generated')
+            print('')
+            input('Press ENTER to exit')        
 
     except requests.exceptions.ConnectionError:
         shutil.rmtree('body')
-        print('Something else went wrong')
-    
-    sleep(0.1)
-    loadbar(page_content.index(i) + 1, count_titles, prefix='PDF-Generating:', suffix='Complete', length=count_titles)
+        print('ConnectionError: Please check your network connection')
+        input('Press ENTER to exit')
 
-
-# Pdf Generation
-for i in page_titles:
-    # Creating createpdf object
-    locals()[i] = CreatePDF()
-    # Add page before writing
-    locals()[i].add_page()
-    # Printing the titles
-    locals()[i].chapter_title(i)
-    # Printing the body
-    locals()[i].print_chapter(f'body/sample_{page_titles.index(i)}.txt')
-    # Generate PDF file
-    locals()[i].output(f'generated/Eng-Essay_{i}.pdf')
-
-# Delete body files
-shutil.rmtree('body')
-
-# Closing Message
-current_dir = os.getcwd()
-print(f'Generated PDF files : {current_dir}/generated')
-print('')
-input('Press ENTER to exit')
+if __name__ == '__main__':
+    pass
 
 
 
